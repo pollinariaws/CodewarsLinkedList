6869e26fa4799e6ef16c8d6ed97e2412426d4057
pollinariaws <polinazuzuk@gmail.com>
2026-03-24 11:02:11 +0200

    Update solution.py
    added the final code

--- Swap Node Pairs/solution.py
+++ Swap Node Pairs/solution.py
@@ -1,1 +1,18 @@
+from preloaded import Node
 
+def swap_pairs(head):
+    if not head or not head.next:
+        return head
+    new_head = head.next
+    prev = None
+    current = head
+    while current and current.next:
+        a = current
+        b = current.next
+        a.next = b.next
+        b.next = a
+        if prev:
+            prev.next = b
+        prev = a
+        current = a.next
+    return new_head
