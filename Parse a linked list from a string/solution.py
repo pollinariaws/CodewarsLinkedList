0e47371d5ac5704ab51ba78b1fba16c20713671f
pollinariaws <polinazuzuk@gmail.com>
2026-03-24 11:05:28 +0200

    Update solution.py
    added the final code

--- Parse a linked list from a string/solution.py
+++ Parse a linked list from a string/solution.py
@@ -1,1 +1,13 @@
+from preloaded import Node
 
+def linked_list_from_string(list_repr: str) -> Node | None:
+    parts = list_repr.split(' -> ')
+    if parts[0] == 'None':
+        return None
+    head = Node(int(parts[0]))
+    current = head
+    for part in parts[1:]:
+        if part != 'None':
+            current.next = Node(int(part))
+            current = current.next
+    return head
