627ebc5d6f3f8283ffc7d6dfa410ff0bc0115cf6
pollinariaws <polinazuzuk@gmail.com>
2026-03-24 11:00:43 +0200

    Update solution.py
    Added the final code

--- Can you get the loop/solution.py
+++ Can you get the loop/solution.py
@@ -1,1 +1,16 @@
-
+def loop_size(node):
+    slow = node
+    fast = node
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+    count = 0
+    current = slow
+    while True:
+        current = current.next
+        count += 1
+        if current == slow:
+            break
+    return count
