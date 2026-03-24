2cb1b076af66f739bc58d56d396cad812960d3de
pollinariaws <polinazuzuk@gmail.com>
2026-03-24 11:06:00 +0200

    Update solution.py
    added the final code

--- Convert a linked list to a string/solution.py
+++ Convert a linked list to a string/solution.py
@@ -1,1 +1,7 @@
-
+def stringify(node):
+    result = []
+    while node is not None:
+        result.append(str(node.data))
+        node = node.next
+    result.append('None')
+    return ' -> '.join(result)
