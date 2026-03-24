7240722346c365f36ba7f3ca29c7075c13f7bcba
pollinariaws <polinazuzuk@gmail.com>
2026-03-24 11:03:07 +0200

    Update solution.py
    added the final code

--- Recursive Reverse/solution.py
+++ Recursive Reverse/solution.py
@@ -1,1 +1,13 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
+def reverse(head):
+    def recurse(current, prev):
+        if not current:
+            return prev
+        next_node = current.next
+        current.next = prev
+        return recurse(next_node, current)
+    return recurse(head, None)
