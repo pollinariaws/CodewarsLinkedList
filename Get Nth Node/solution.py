2e74060a6ff5be2544d1a8f2e86386498e9c0115
pollinariaws <polinazuzuk@gmail.com>
2026-03-24 11:04:32 +0200

    Update solution.py
    added the final code

--- Get Nth Node/solution.py
+++ Get Nth Node/solution.py
@@ -1,1 +1,17 @@
+from preloaded import Node
 
+# class Node(object):
+#     """Node class for reference"""
+#     def __init__(self, data, next=None):
+#         self.data = data
+#         self.next = next
+    
+def get_nth(node, index):
+    current = node
+    current_idx = 0
+    while current is not None:
+        if current_idx == index:
+            return current
+        current = current.next
+        current_idx += 1
+    raise Exception("Index out of range")
