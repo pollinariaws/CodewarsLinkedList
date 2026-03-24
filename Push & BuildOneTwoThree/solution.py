62e2d1afa9dd5a2dba993cb44a8357fa628662d1
pollinariaws <polinazuzuk@gmail.com>
2026-03-24 11:05:02 +0200

    Update solution.py
    added the final code

--- Push & BuildOneTwoThree/solution.py
+++ Push & BuildOneTwoThree/solution.py
@@ -1,1 +1,22 @@
+from preloaded import Node
 
+'''
+Node is defined in preloaded like this:
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+    
+def push(head, data):
+    node = Node(data)
+    node.next = head
+    return node
+
+def build_one_two_three():
+    result = None
+    result = push(result, 3)
+    result = push(result, 2)
+    result = push(result, 1)
+    return result
